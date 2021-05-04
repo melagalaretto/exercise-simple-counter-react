fcec1e2d7baf8710b7b88fc2c549e156dbaa9d86
melagalaretto <moonsea1711@gmail.com>
2021-05-04 19:19:01 +0000

    Subida de archivo ultima

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,10 +7,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home(props) {
 	return (
-		/* Animo :) vos podes, yo creo en vos */
-		<div className="container bg-dark text-white" id="contenedor">
+		<div
+			className="container bg-dark text-white text-center"
+			id="contenedor">
 			<div className="row">
-				<div className="col-2 h2 text-center" id="clock">
+				<div className="col-2 h2" id="clock">
 					<i className="fas fa-clock"></i>
 				</div>
 				<div id="ultseg">
